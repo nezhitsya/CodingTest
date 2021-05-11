3db33e102501fd79df9647a0dc771c973b95c7d7
nezhitsya <huyn9295@naver.com>
2021-05-11 16:26:07 +0900

    set up chapter 10

---
+++ Chapter10/02.py
@@ -0,0 +1,34 @@
+def find_parent(parent, x) :
+    # 루트 노드가 아닌 경우, 루트 노드를 찾을 때까지 재귀 호출
+    if parent[x] != x :
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+# 두 원소가 속한 집합 union
+def union_parent(parent, a, b) :
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b :
+        parent[b] = a
+    else :
+        parent[a] = b
+
+v, e = map(int, input().split()) # 노드, 간선 개수
+parent = [0] * (v + 1) # 부모 테이블 초기화
+
+for i in range(1, v + 1) :
+    parent[i] = i # 부모를 자기 자신으로 초기화
+
+for i in range(e) :
+    a, b = map(int, input().split())
+    union_parent(parent, a, b)
+
+print('각 원소가 속한 집합 : ', end = '')
+for i in range(1, v + 1) :
+    print(find_parent(parent, i), end = ' ')
+
+print()
+
+print('부모 테이블 : ', end = '')
+for i in range(1, v + 1) :
+    print(parent[i], end = ' ')
