9a74484046a71c32702a7ed21a82575c90003039
nezhitsya <huyn9295@naver.com>
2021-10-18 17:34:59 +0900

    set up boj

--- BOJ/2579.py
+++ BOJ/2579.py
@@ -1,15 +1,13 @@
-n = int(input())
-stair = list()
+import sys
 
-for i in range(n):
-    stair.append(int(input()))
+read = sys.stdin.readline
+n = int(read())
+stair = [0] + [int(read()) for _ in range(n)] + [0]
+cache = [0] * (n + 2)
+cache[1] = stair[1]
+cache[2] = cache[1] + stair[2]
 
-score = list()
-score.append(stair[0])
-score.append(stair[0] + stair[1])
-score.append(max(stair[2] + stair[0], stair[2] + stair[1]))
+for i in range(3, n + 1):
+    cache[i] = max(cache[i - 2], cache[i - 3] + stair[i - 1]) + stair[i]
 
-for i in range(3, n):
-    score.append(max(stair[i] + score[i - 2], stair[i] + score[i - 1] + score[i - 3]))
-
-print(score[0]) 
+print(cache[n])
