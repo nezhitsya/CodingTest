3c46029dbf5c20f883d09373284f0910c71a909f
nezhitsya <huyn9295@naver.com>
2021-10-31 22:47:55 +0900

    set up boj

--- BOJ/12865.py
+++ BOJ/12865.py
@@ -1,10 +1,20 @@
-n, w = 4, 5
-bag = [(2, 3), (3, 4), (4, 5), (5, 6)]
-knap = [0 for _ in range(w + 1)]
+n, k = map(int, input().split())
+dp = [[0 for _ in range(k + 1)] for _ in range(n + 1)]
+w = [0]
+v = [0]
 
 for i in range(n):
-    for j in range(w, 1, -1):
-        if bag[i][0] <= j:
-            knap[j] = max(knap[j], knap[j - bag[i][0]] + bag[i][1])
+    a, b = map(int, input().split())
+    w.append(a)
+    w.append(b)
 
-print(knap)
+for i in range(1, n + 1):
+    for j in range(1, k + 1):
+        weight = w[i]
+        value = v[i]
+        if j < weight:
+            dp[i][j] = dp[i - 1][j]
+        else :
+            dp[i][j] = max(dp[i - 1][j - weight] + value, dp[i - 1][j])
+
+print(dp[n][k])
