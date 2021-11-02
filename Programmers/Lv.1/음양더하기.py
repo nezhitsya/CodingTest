c166d643af1c9c3f80877dad295650e1b74989ed
nezhitsya <huyn9295@naver.com>
2021-11-02 14:43:17 +0900

    set up prog

---
+++ Programmers/Lv.1/음양더하기.py
@@ -0,0 +1,11 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i]:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
+    return answer
+
+# def solution(absolutes, signs):
+#     return sum(absolutes if sign else -absolutes for absolutes, sign in zip(absolutes, signs))
