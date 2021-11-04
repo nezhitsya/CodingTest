e4a3cfc7a67c195c9d77d5080b08a6beda4a5fef
nezhitsya <huyn9295@naver.com>
2021-11-04 12:21:05 +0900

    set up prog

---
+++ Programmers/Lv.1/가운데글자가져오기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = ''
+    center = len(s) // 2
+    
+    if len(s) % 2 != 0:
+        answer = s[center]
+    else:
+        answer = s[center - 1: center + 1]
+        
+    return answer
+
+# def string_middle(str):
+#     return str[(len(str) - 1) // 2 : len(str) // 2 + 1]
---
+++ Programmers/Lv.1/나누어떨어지는숫자배열.py
@@ -0,0 +1,16 @@
+def solution(arr, divisor):
+    answer = []
+    
+    for i in arr:
+        if i % divisor == 0:
+            answer.append(i)
+    
+    if not answer:
+        answer.append(-1)
+    else:
+        answer.sort()
+        
+    return answer
+
+# def solution(arr, divisor): 
+#     return sorted([n for n in arr if n % divisor == 0]) or [-1]
