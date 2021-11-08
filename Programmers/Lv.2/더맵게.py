aa1c0b29cd307c61ac3fa410ec795bb0ef87d84b
nezhitsya <huyn9295@naver.com>
2021-11-08 17:15:08 +0900

    set up prog

--- Programmers/Lv.2/더맵게.py
+++ Programmers/Lv.2/더맵게.py
@@ -1,12 +1,13 @@
-def solution(scoville, K):
-    answer = 0
+# Time Out
+# def solution(scoville, K):
+#     answer = 0
     
-    while min(scoville) < K:
-        scoville.sort()
-        try:
-            scoville.append(scoville.pop(0) + scoville.pop(0) * 2)
-        except IndexError:
-            return -1
-        answer += 1
+#     while min(scoville) < K:
+#         scoville.sort()
+#         try:
+#             scoville.append(scoville.pop(0) + scoville.pop(0) * 2)
+#         except IndexError:
+#             return -1
+#         answer += 1
     
-    return answer
+#     return answer
