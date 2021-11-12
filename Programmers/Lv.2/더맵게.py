64bd9fa28ba51a35c292a71caa120a59e0028201
nezhitsya <huyn9295@naver.com>
2021-11-12 16:47:20 +0900

    set up prog

--- Programmers/Lv.2/더맵게.py
+++ Programmers/Lv.2/더맵게.py
@@ -1,3 +1,17 @@
+from heapq import *
+
+def solution(scoville, K):
+    answer = 0
+    heapify(scoville)
+    
+    while scoville[0] < K and len(scoville) > 1:
+        num1 = heappop(scoville)
+        num2 = heappop(scoville)
+        heappush(scoville, num1 + num2 * 2)
+        answer += 1
+    
+    return answer if scoville[0] >= K else -1
+
 # Time Out
 # def solution(scoville, K):
 #     answer = 0
