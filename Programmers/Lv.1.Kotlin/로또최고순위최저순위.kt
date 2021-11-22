fd54c9bb573b678527cd57c91851c76d38f158c2
nezhitsya <huyn9295@naver.com>
2021-11-22 17:24:25 +0900

    set up prog

---
+++ Programmers/Lv.1.Kotlin/로또최고순위최저순위.kt
@@ -0,0 +1,38 @@
+class Solution {
+    fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
+        var answer: IntArray = intArrayOf(0, 0)
+        var count = 0
+        var num = 0
+        
+        for (i in lottos.indices) {
+            if (lottos[i] == 0) {
+                count++
+            } else {
+                if (win_nums.contains(lottos[i])) {
+                    num++
+                }
+            }
+        }
+        
+        answer[0] = checkNum(num + count)
+        answer[1] = checkNum(num)
+        
+        return answer
+    }
+    
+    fun checkNum(lotto: Int): Int {
+        if (lotto == 6) {
+            return 1
+        } else if (lotto == 5) {
+            return 2
+        } else if (lotto == 4) {
+            return 3
+        } else if (lotto == 3) {
+            return 4
+        } else if (lotto == 2) {
+            return 5
+        } else {
+            return 6
+        }
+    }
+}
